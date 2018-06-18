7f07fcd08a9f1cf1eb408a88416bb53f513f6231
jesuspisa <jesus.delapisa@opendeusto.es>
2018-06-18 13:28:15 +0200

    Añadiendo traduccion de idiomas

--- src/main/java/es/deusto/spq/ciudades/client/gui/VentanLogin.java
+++ src/main/java/es/deusto/spq/ciudades/client/gui/VentanLogin.java
@@ -2,24 +2,41 @@ package es.deusto.spq.ciudades.client.gui;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JTextField;
+
+import org.apache.log4j.Logger;
 
 import es.deusto.spq.ciudades.client.controller.CiudadesController;
 
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.ResourceBundle;
 import java.awt.event.ActionEvent;
 
 public class VentanLogin extends JFrame {
 	
-	private CiudadesController controler;
+	final static Logger logger = Logger.getLogger(VentanLogin.class);
+	
+	private static final long serialVersionUID = 1L;
+	
+	private CiudadesController controller;
 	
 
 	private JTextField textFieldUsuario;
 	private JTextField textFieldPassword;
 	
-	public VentanLogin() {
+	protected ResourceBundle ResourceBundle;
+	
+	public VentanLogin(CiudadesController controller, final ResourceBundle resourceBundle) {
+		
+		logger.info("VentanaLogin");
+		
+		
+		
+		
 		setBounds(500, 100, 365, 200);
 		
 		
@@ -48,9 +65,24 @@ public class VentanLogin extends JFrame {
 		getContentPane().add(textFieldPassword);
 		textFieldPassword.setColumns(10);
 		
-		JButton btnEntrar = new JButton("Entrar");
+		JButton btnEntrar = new JButton("Iniciar sesión");
 		btnEntrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				if(controller.identifyUsuario(String.valueOf(textFieldUsuario.getText()), String.valueOf(textFieldPassword.getText()))) {
+				
+					
+				}
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
 				if (textFieldUsuario.getText()=="admin"&&textFieldPassword.getText()=="admin") {
 					//abrir  la ventana del administrados para introducir ciudades nuevas
 				}else {
@@ -65,15 +97,37 @@ public class VentanLogin extends JFrame {
 		JButton btnRegistrarse = new JButton("Registrarse");
 		btnRegistrarse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				VentanaCrearPerfil registrarse = new VentanaCrearPerfil(controler);
+				VentanaCrearPerfil registrarse = new VentanaCrearPerfil(controller.identifyUsuario(email, password));
 				registrarse.setVisible(true);
 				dispose();
 			}
 		});
-		btnRegistrarse.setBounds(259, 126, 89, 23);
+		btnRegistrarse.setBounds(234, 126, 114, 23);
 		getContentPane().add(btnRegistrarse);
 	}
-	public static void main(String[] args) {
-		VentanLogin a = new VentanLogin();
-		a.setVisible(true);	}
+
+	/**
+	 * Metodo que limpia los campos de la ventana login.
+	 */
+	private void limpiarCampos() {
+		textFieldUsuario.setText("");
+		textFieldPassword.setText("");
+	}
+
+	/**
+	 * Metodo get que devuelve el controlador.
+	 * @return controller
+	 */
+	public CiudadesController getController() {
+		return controller;
+	}
+
+	/**
+	 * Metodo set que cambia el controlador. 
+	 * @param controller controlador
+	 */
+	public void setController(CiudadesController controller) {
+		this.controller = controller;
+	}
+	
 }
